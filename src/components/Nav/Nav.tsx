d084aecef7966d180dda27ca7dc7ccfd629efc91
jonathanengelbert <jonathanengelbert@gmail.com>
2020-06-22 13:25:54 -0400

    Changes navStyle to responsive

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+
+// MATERIAL UI config
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+
+import './navStyles.scss';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -31,7 +35,7 @@ export default function Nav() {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        {/*JE*/}
+                        <p className={"logo"}>Jonathan Engelbert</p>
                     </Typography>
                     <Button color="inherit">Resume</Button>
                     <Button color="inherit">Contact</Button>
